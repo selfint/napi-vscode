ec8f84b7440b97bb240170b6506d86ac2a032601
Tom Selfin <selfint@gmail.com>
2023-06-25 20:51:17 +0300

    connect core to extension

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,12 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+import * as core from "@napi-vscode/core";
+
+export function helloFromCore(): string {
+	return `Hello from napi-vscode! 2 + 2 = ${core.sum(2, 2)}`;
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -16,7 +22,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('napi-vscode.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from napi-vscode!');
+		vscode.window.showInformationMessage(helloFromCore());
 	});
 
 	context.subscriptions.push(disposable);
